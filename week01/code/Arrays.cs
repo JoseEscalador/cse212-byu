5c5a4b6a44835aa6bced8709bb037fe38319e29d
JoseEscalador <luisjpinheiro@gmail.com>
2026-09-05 10:01:31 -0300

    .

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+
+
 public static class Arrays
 {
     /// <summary>
@@ -22,7 +24,7 @@ public static class Arrays
             multiples[i] = (i + 1) * number;
         }
         // get the resulting array
-        return multiples; 
+        return multiples;
     }
 
     /// <summary>
@@ -38,5 +40,18 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // create a second list to work with
+        List<int> data2 = new(data.Count);
+        // create a start variable to get the first part
+        int start = data.Count - amount;
+        // get the first part with getRange before the second
+        data2.AddRange(data.GetRange(start, amount));
+        // get the second part
+        data2.AddRange(data.GetRange(0, start));
+        // clear data to repopulate it
+        data.Clear();
+        // repopulate data
+        data.AddRange(data2);
     }
 }
